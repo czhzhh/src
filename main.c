1de2ca8f8abc7bd48585e304c18e4dc77d13210c
lujiaji <fxmxjx720@gmail.com>
2024-12-02 16:30:09 -0800

    修改了main.c中的定义顺序，解决了lab2b状态机不能正常编译的问题

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "platform.h"
 #include "xil_printf.h"
 #include "xil_cache.h"
+#include "qpn_port.h"
 #include "bsp.h"
 #include "complex.h"
 #include "fft.h"
@@ -10,22 +11,23 @@
 #include "my_rand.h"
 #include "sevenSeg_new.h"
 #include "trig.h"
-#include "qpn_port.h"
+
 #include "dspl_upd.h"
 
-static QEvent 1_lab2bQueue[30];
+static QEvent l_lab2bQueue[30];
 
-QActiveCB const Q_ROM Q_ROM_VAR QF_active[]={
-		{ (QActive *)0,            (QEvent *)0,          0                    },
-			{ (QActive *)&l2b,    l_lab2bQueue,         Q_DIM(l_lab2bQueue)  }
+QActiveCB const Q_ROM Q_ROM_VAR QF_active[] = {
+	{ (QActive *)0,            (QEvent *)0,          0                    },
+	{ (QActive *)&l2b,    l_lab2bQueue,         Q_DIM(l_lab2bQueue)  }
 };
+
 Q_ASSERT_COMPILE(QF_MAX_ACTIVE == Q_DIM(QF_active) - 1);
 volatile int timerTrigger = 0;
+
 void TimerCounterHandler(void *CallBackRef, u8 TmrCtrNumber)
 {
 	timerTrigger = 100;
 }
-
 int main()
 {
 	Xil_ICacheInvalidate();
