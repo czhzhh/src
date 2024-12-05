bb15c424ff6f460a976b0ce5fc7739d210bcc0f2
lujiaji <fxmxjx720@gmail.com>
2024-12-04 20:36:36 -0800

    update displayer and logic

--- displayer.h
+++ displayer.h
@@ -1,19 +1,19 @@
 #ifndef displayer_h
 #define displayer_h
 
-//int former_x=0;
-//int former_y=0;
+
+#include "logic.h"
 
 void Init_game();
 void init_ball_pixel_counts();
-void draw_ball(int x, int y, int r);
+void draw_ball(Ball *ball);
 void DrawBorder();
 void Set_Blue();
 void Tmr_Set_Blue(void);
 void Txt_Set_BLUE(void);
 void dspl_init();
 void dspl_Settings();
-void erase_former();
+void erase_former(int former_x,int former_y,int r);
 void Init_Bricks(void);
 void Bricks(int count, int *positions);
 void update();
